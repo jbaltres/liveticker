bc24239bf08424125f2e960071b2ba996211a00c
Juergen Baltres <juergen.baltres@gmail.com>
2019-10-23 16:10:06 +0200

    add routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
 import MainScreen from "./components/MainScreen";
 import GlobalStyles from "./GlobalStyles";
+import Countdown from "./pages/Countdown";
+import AddNewCountdown from "./pages/AddNewCountdown";
+import DayEvents from "./pages/DayEvents";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./components/Header";
+import MenuContainer from "./components/MenuContainer";
 
 function App() {
+  const [open, setOpen] = React.useState(false);
+
   return (
-    <>
+    <Router>
       <GlobalStyles />
-      <MainScreen />
-    </>
+      <Header open={open} setOpen={setOpen} />
+      <MenuContainer open={open} />
+      <Route path="/" exact component={Countdown} />
+      <Route path="/new" component={AddNewCountdown} />
+      <Route path="/dayevents" component={DayEvents} />
+    </Router>
   );
 }
 
---
+++ src/pages/DayEvents.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function DayEvents() {
+  return <h1>At some Point of Time some Content will Follow</h1>;
+}
