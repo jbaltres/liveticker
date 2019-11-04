aa7f7c0f6cc48a2ce707d2e78e94f18bb6d01143
Juergen Baltres <juergen.baltres@gmail.com>
2019-11-04 13:59:17 +0100

    change modalcode after review

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,10 +1,7 @@
 import React from "react";
 import styled from "styled-components";
-import Inputfields from "./Inputfield";
-import TimeInputfield from "./TimeInputfield";
 import InputWrapper from "./InputWrapper";
 import ActionButton from "./Button";
-import Axios from "axios";
 import { Link } from "react-router-dom";
 
 const ModalWrapper = styled.div`
@@ -13,98 +10,20 @@ const ModalWrapper = styled.div`
   margin-top: 30%;
 `;
 
-const StyledParagraph = styled.p`
-  justify-content: center;
-  color: #f6f6f4;
-  margin: 0px;
-  font-size: 20px;
-`;
-
-const StyledLink = styled(Link)`
-  text-decoration: none;
-`;
-
-function Modal() {
-  function addToJsonDb() {
-    Axios.post("http://localhost:3001/locations", {
-      time: timeValue,
-      headline: formValue,
-      subheadline: formValue2,
-      entranceFee: formValue3,
-      locationName: formValue4,
-      adress: formValue5
-    })
-      .then(response => {
-        console.log(response);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
-
-  const [timeValue, setTimeValue] = React.useState("");
-  const [formValue, setFormValue] = React.useState("");
-  const [formValue2, setFormValue2] = React.useState("");
-  const [formValue3, setFormValue3] = React.useState("");
-  const [formValue4, setFormValue4] = React.useState("");
-  const [formValue5, setFormValue5] = React.useState("");
-  const [formValue6, setFormValue6] = React.useState("");
-
+function Modal({ children, submitForm, pw }) {
   return (
     <ModalWrapper>
       <InputWrapper>
         <ActionButton>X</ActionButton>
-        <TimeInputfield
-          value={timeValue}
-          onChange={event => setTimeValue(event.target.value)}
-          placeholder="99"
-          maxLength="3"
-        />
-        <StyledParagraph>Seconds</StyledParagraph>
-        <Inputfields
-          value={formValue}
-          onChange={event => setFormValue(event.target.value)}
-          placeholder="Special Name"
-          name="name"
-        />
-        <Inputfields
-          value={formValue2}
-          onChange={event => setFormValue2(event.target.value)}
-          placeholder="Special Description"
-        />
-        <Inputfields
-          value={formValue3}
-          onChange={event => setFormValue3(event.target.value)}
-          placeholder="Entrance Fee"
-        />
-        <Inputfields
-          value={formValue4}
-          onChange={event => setFormValue4(event.target.value)}
-          placeholder="Location Name"
-        />
-        <Inputfields
-          value={formValue5}
-          onChange={event => setFormValue5(event.target.value)}
-          placeholder="Adress"
-        />
-        {/* <Inputfields
-          type="password"
-          onChange={event => setFormValue6(event.target.value)}
-          placeholder="Password"
-        /> */}
-        <StyledLink to="/">
-          <ActionButton bgColor={true} handleClick={() => addToJsonDb()}>
+        {children}
+        <Link to="/">
+          <ActionButton bgColor={true} onClick={submitForm}>
             √
           </ActionButton>
-        </StyledLink>
+        </Link>
       </InputWrapper>
     </ModalWrapper>
   );
 }
 
 export default Modal;
-
-// function for key value pair objects
-// function getInputValue(key, value){
-//   setInput(input[key] = value);
-// }
--- src/pages/AddNewCountdown.js
+++ src/pages/AddNewCountdown.js
@@ -1,6 +1,85 @@
 import React from "react";
 import Modal from "../components/Modal";
+import TimeInputfield from "../components/TimeInputfield";
+import Inputfields from "../components/Inputfield";
+import styled from "styled-components";
+import axios from "axios";
+
+const StyledParagraph = styled.p`
+  justify-content: center;
+  color: #f6f6f4;
+  margin: 0px;
+  font-size: 20px;
+`;
 
 export default function AddNewCountdown() {
-  return <Modal />;
+  const [timeValue, setTimeValue] = React.useState("");
+  const [headerValue, setHeaderValue] = React.useState("");
+  const [descriptionValue, setDescriptionValue] = React.useState("");
+  const [entranceFeeValue, setEntranceFeeValue] = React.useState("");
+  const [locationNameValue, setLocationNameValue] = React.useState("");
+  const [adressValue, setAdressValue] = React.useState("");
+  const [passwordValue, setPasswordValue] = React.useState("");
+
+  function addToJsonDb() {
+    axios
+      .post("/locations", {
+        time: timeValue,
+        headline: headerValue,
+        subheadline: descriptionValue,
+        entranceFee: entranceFeeValue,
+        locationName: locationNameValue,
+        adress: adressValue
+      })
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  return (
+    <Modal submitForm={addToJsonDb}>
+      <TimeInputfield
+        value={timeValue}
+        onChange={event => setTimeValue(event.target.value)}
+        placeholder="99"
+        maxLength="3"
+      />
+      <StyledParagraph>Seconds</StyledParagraph>
+      <Inputfields
+        value={headerValue}
+        onChange={event => setHeaderValue(event.target.value)}
+        placeholder="Special Name"
+      />
+      <Inputfields
+        value={descriptionValue}
+        onChange={event => setDescriptionValue(event.target.value)}
+        placeholder="Special Description"
+      />
+      <Inputfields
+        value={entranceFeeValue}
+        onChange={event => setEntranceFeeValue(event.target.value)}
+        placeholder="Entrance Fee"
+      />
+      <Inputfields
+        value={locationNameValue}
+        onChange={event => setLocationNameValue(event.target.value)}
+        placeholder="Location Name"
+      />
+      <Inputfields
+        value={adressValue}
+        onChange={event => setAdressValue(event.target.value)}
+        placeholder="Address"
+      />
+      <Inputfields
+        type="password"
+        placeholder="Password"
+        value={passwordValue}
+        onChange={event => setPasswordValue(event.target.value)}
+        pw={passwordValue}
+      />
+    </Modal>
+  );
 }
